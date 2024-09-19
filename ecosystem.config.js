42472ce204087155f629b45855936f3d62bd9043
inseokLee999 <syt06137@naver.com>
2024-09-19 15:45:03 +0900

    임시 설정 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'next-app',
+      name: 'frontend-admin',
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 6007',
       instances: 1,
